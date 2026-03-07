74e3571fee73f005d0be83c0b4385b588acc739a
Anvesh <127006149+anveshpol1522@users.noreply.github.com>
2026-03-08 01:21:43 +0530

    Delete Module 2 Node.js + React directory

--- Module 2 Node.js + React/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './petpooja-copilot.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
